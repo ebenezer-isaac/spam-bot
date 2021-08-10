29d61e78bc02aa48381d5100ee4c26720089d2c7
Ebenezer Isaac <mail@ebenezer-isaac.com>
2021-08-10 10:29:44 +0530

    shell spammer

---
+++ spammer.sh
@@ -0,0 +1,33 @@
+sleep 5
+for wood in {1..100}
+do
+    xdotool key t
+    sleep 0.1
+    xdotool type 'bring me my axe'
+    sleep 0.01
+    xdotool key Return
+done
+for food in {1..100}
+do
+    xdotool key t
+    sleep 0.1
+    xdotool type 'i want pizza'
+    sleep 0.01
+    xdotool key Return
+done
+for metal in {1..100}
+do
+    xdotool key t
+    sleep 0.1
+    xdotool type 'your money or your life'
+    sleep 0.01
+    xdotool key Return
+done
+for stone in {1..100}
+do
+    xdotool key t
+    sleep 0.1
+    xdotool type 'i see a mountain here'
+    sleep 0.01
+    xdotool key Return
+done
